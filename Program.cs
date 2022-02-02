dd4f7d002952938b0ae27dce2334db65744dddc9
Cesar Trujillo <catg20g@gmail.com>
2022-02-01 18:15:37 -0800

    Automatizador git

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics;
 
-//int menu = 0;
 
 Console.WriteLine("Bienvenido al git automatico");
 Console.WriteLine("|------------------------------------|");
@@ -9,7 +8,16 @@ Console.WriteLine("|2.- Subir cambios                   |");
 Console.WriteLine("|3.- Cambiar a main                  |");
 Console.WriteLine("|------------------------------------|");
 
+Process cmd = new Process();
+cmd.StartInfo.FileName = "cmd.exe";
+cmd.StartInfo.CreateNoWindow = true;
+cmd.StartInfo.RedirectStandardInput = true;
+cmd.StartInfo.RedirectStandardOutput = true;
+cmd.StartInfo.UseShellExecute = false;
+cmd.Start();
+cmd.StandardInput.Write("echo hello world!");
+cmd.StandardInput.Flush();
+cmd.StandardInput.Close();
+Console.WriteLine(cmd.StandardOutput.ReadToEnd());
 
-
-ProcessStartInfo cmd = new ProcessStartInfo();
-cmd.FileName = "cmd.exe";
+Console.ReadKey();
